8927e35a7ca565dea9ad39ff8ca577264257ab65
droidsharks2024 <droidsharks2024@gmail.com>
2024-10-17 18:21:18 -0700

    changes made oct 17th

--- M1_M4_coral_nursery_scuba_diver.py
+++ M1_M4_coral_nursery_scuba_diver.py
@@ -1,6 +1,10 @@
-'''
-   This script is written for mission 1: Coral nursery and mission 4: scuba diver
-'''
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
 
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
@@ -19,18 +23,38 @@ attachment_angle = attachment.angle()
 drive_base = DriveBase(left_wheel, right_wheel, wheel_diameter=56, axle_track=112)
 drive_base.use_gyro(True)
 
+
 #mission 1 coral buds
 drive_base.straight(300)
 drive_base.turn(15)
 drive_base.straight(375)
 drive_base.turn(-90)
 attachment.run(25)
-drive_base.straight(75)
+wait(1000)
+drive_base.straight(130)
 
 #mission 4 beginning
-attachment.run(100)
+wait(1000)
+attachment.run(70)
 wait(1000)
 drive_base.straight(-100)
 drive_base.turn(80)
 attachment.run(-45)
-drive_base.straight(100)
+wait(1000)
+attachment.run(-60)
+wait(1000)
+drive_base.straight(80)
+drive_base.straight(10)
+
+'''
+#coral tree
+drive_base.straight(200)
+wait(1000)
+drive_base.straight(-320)
+wait(1000)
+drive_base.straight(140)
+wait(1000)
+drive_base.straight(400)
+wait(1000)
+#drive_base.straight(-300)
+'''
